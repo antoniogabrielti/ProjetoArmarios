1875cc82572d5e68bc79e26dcbeaf9d0ebb2857f
Antonio Gabriel Miranda <antoniogabrielti@gmail.com>
2016-09-25 22:27:36 -0300

    Acrescentado CRUD Armarios

---
+++ SistemaArmarios/src/java/model/Armario.java
@@ -0,0 +1,26 @@
+
+package model;
+
+
+public class Armario {
+    private int numero;
+    private boolean estaDisponivel;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public boolean isEstaDisponivel() {
+        return estaDisponivel;
+    }
+
+    public void setEstaDisponivel(boolean estaDisponivel) {
+        this.estaDisponivel = estaDisponivel;
+    }
+    
+    
+}
