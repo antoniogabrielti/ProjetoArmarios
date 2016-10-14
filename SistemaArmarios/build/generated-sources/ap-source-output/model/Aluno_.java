3a3a87476e5a9ebcdd14e24fb6f7fe561b5730e2
Antonio Gabriel Miranda <antoniogabrielti@gmail.com>
2016-10-14 09:13:48 -0300

    Crud JPA de Armarios e Alunos

---
+++ SistemaArmarios/build/generated-sources/ap-source-output/model/Aluno_.java
@@ -0,0 +1,16 @@
+package model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-13T21:04:15")
+@StaticMetamodel(Aluno.class)
+public class Aluno_ extends Usuario_ {
+
+    public static volatile SingularAttribute<Aluno, String> telefone;
+    public static volatile SingularAttribute<Aluno, String> matricula;
+    public static volatile SingularAttribute<Aluno, String> nome;
+    public static volatile SingularAttribute<Aluno, String> email;
+
+}
