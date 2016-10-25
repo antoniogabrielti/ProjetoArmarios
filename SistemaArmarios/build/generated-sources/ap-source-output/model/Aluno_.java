b7392d819fa1e61ba6689998a2b67f2a967008a8
Antonio Gabriel Miranda <antoniogabrielti@gmail.com>
2016-10-25 02:15:58 -0200

    Atualizado 25/10 - Inserido busca com filtro armario, botão cancelar opções e fluxo alternativo no emprestimo

---
+++ SistemaArmarios/build/generated-sources/ap-source-output/model/Aluno_.java
@@ -0,0 +1,16 @@
+package model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-25T00:41:01")
+@StaticMetamodel(Aluno.class)
+public class Aluno_ extends Usuario_ {
+
+    public static volatile SingularAttribute<Aluno, String> telefone;
+    public static volatile SingularAttribute<Aluno, String> matricula;
+    public static volatile SingularAttribute<Aluno, String> nome;
+    public static volatile SingularAttribute<Aluno, String> email;
+
+}
---
+++ SistemaArmarios/src/java/model/Emprestimo.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author juliano
+ */
+@Entity
+@Table(name="emprestimo")
+public class Emprestimo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @OneToOne
+    private Armario armario;
+    @OneToOne
+    private Aluno proprietario;
+    @Temporal (value=TemporalType.DATE)
+    private Date dataEmprestimo;
+    @Temporal (value=TemporalType.DATE)
+    private Date dataDevolucao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Armario getArmario() {
+        return armario;
+    }
+
+    public void setArmario(Armario armario) {
+        this.armario = armario;
+    }
+
+    public Aluno getProprietario() {
+        return proprietario;
+    }
+
+    public void setProprietario(Aluno proprietario) {
+        this.proprietario = proprietario;
+    }
+
+    public Date getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(Date dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public Date getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(Date dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Emprestimo)) {
+            return false;
+        }
+        Emprestimo other = (Emprestimo) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.Emprestimo[ id=" + id + " ]";
+    }
+    
+}
